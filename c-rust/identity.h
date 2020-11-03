d01a192f48f08a1c72879b5f55fe3dc5539b8d6d
Tyler Connel <tconnel3@gmail.com>
2020-11-02 23:32:18 -0700

    got the basic rust-ffi working from c->rust.

---
+++ c-rust/identity.h
@@ -0,0 +1,8 @@
+#ifndef LIB_C_RUST_H
+#define LIB_C_RUST_H
+
+#include <stdint.h>
+
+extern int32_t identity(int32_t);
+
+#endif /* LIB_C_RUST_H */
---
+++ c-rust/main.c
@@ -0,0 +1,5 @@
+#include "identity.h"
+
+int main(int argc, char* argv[]) {
+    return identity(0);
+}
