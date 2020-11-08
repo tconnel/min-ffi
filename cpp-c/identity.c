985de5327cedfd9617832ae6a68c25ea76661968
Tyler Connel <tconnel3@gmail.com>
2020-11-07 17:26:58 -0700

    cpp -> c linking even easier.

---
+++ cpp-c/identity.c
@@ -0,0 +1,8 @@
+#include "identity.h"
+
+#define IDENTITY_BODY(__type)\
+    __type identity_ ## __type(__type x) { return x; }
+
+IDENTITY_BODY(int)
+IDENTITY_BODY(int32_t);
+IDENTITY_BODY(identity_test);
---
+++ cpp-c/main.cpp
@@ -0,0 +1,8 @@
+
+extern "C" {
+#include "identity.h"
+}
+
+int main(int argc, char* argv[]) {
+    return identity_int(0);
+}
