d047be31088376b97bb00cb9475fb2da7e9185a4
Tyler Connel <tconnel3@gmail.com>
2021-05-09 12:30:46 -0700

    Started to understand what's available for the C->SBCL ecosystem. Finding not much support for native binary.

---
+++ c-sbcl/hello_world.lisp
@@ -0,0 +1,10 @@
+; all equivalent streams
+;; (princ "hello world")
+;; (terpri)
+;; 
+;; (write (format nil "hello world~&") :escape nil :readably nil)
+;; 
+;; (princ (format nil "hello world~&"))
+
+(defun id (x) x)
+(sb-ext:save-lisp-and-die :executable t :purify t)
