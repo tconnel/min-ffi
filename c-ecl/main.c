bbadada724fe2ab44734be81273b3518e9e76399
Tyler Connel <tconnel3@gmail.com>
2021-05-09 16:55:58 -0700

    Pausing work on C->ECL. Going to do ECL->C instead.

--- c-ecl/main.c
+++ c-ecl/main.c
@@ -6,8 +6,15 @@ extern cl_object LC2id_int(cl_object); //NOTE never resolves...
 int main(int argc, char* argv[]) {
   cl_boot(argc, argv);
   ecl_init_module(NULL, init_identity);
-  cl_object val = ecl_make_fixnum(4);
-  cl_object outval = LC2id_int(val);
+
+//   cl_object val = ecl_make_fixnum(4);
+//   cl_object squaredval = ecl_read_from_cstring("(* x y");
+//   cl_object fresult = cl_funcall(1, LC2id_int, ecl_make_fixnum(5));
+//   unsigned int outval = ecl_fixnum(val);
+
+//   cl_object val = ecl_make_fixnum(4);
+//   cl_object otherval = cl_funcall(1, (cl_object)LC2id_int, val);
+
   cl_shutdown();
   return 0;
 }
