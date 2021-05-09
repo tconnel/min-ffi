1387304f30f3b1da43b45a52b1cdefc00de77c0a
Tyler Connel <tconnel3@gmail.com>
2021-05-09 16:44:36 -0700

    Finally giving in on calling CL from C. Better to just use the interpreter internally.

--- c-ecl/main.c
+++ c-ecl/main.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <ecl/ecl.h>
 extern void init_identity(cl_object);
-extern cl_object LC2id_int(cl_object);
+extern cl_object LC2id_int(cl_object); //NOTE never resolves...
 
 int main(int argc, char* argv[]) {
   cl_boot(argc, argv);
   ecl_init_module(NULL, init_identity);
-  //cl_object val = LC2id_int(ecl_make_fixnum(4));
-  //ecl_make_fixnum(5)
-  printf("%ld\n", 1l);
+  cl_object val = ecl_make_fixnum(4);
+  cl_object outval = LC2id_int(val);
   cl_shutdown();
   return 0;
 }
