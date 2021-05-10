9a83f1a9382905a93e19875e91ca238b1c9b9509
Tyler Connel <tconnel3@gmail.com>
2021-05-09 18:35:56 -0700

    Use the ECL internals for C->ECL interactions.

--- c-ecl/main.c
+++ c-ecl/main.c
@@ -2,17 +2,10 @@
 extern void call_foo();
 int main(int argc, char* argv[]) {
   cl_boot(argc, argv);
-
-  call_foo();
-
-//   cl_object val = ecl_make_fixnum(4);
-//   cl_object squaredval = ecl_read_from_cstring("(* x y");
-//   cl_object fresult = cl_funcall(1, LC2id_int, ecl_make_fixnum(5));
-//   unsigned int outval = ecl_fixnum(val);
-
-//   cl_object val = ecl_make_fixnum(4);
-//   cl_object otherval = cl_funcall(1, (cl_object)LC2id_int, val);
-
+  cl_object val = ecl_make_fixnum(4);
+  if( ecl_t_of(val) == t_fixnum ) {
+    printf("%ld\n", fix(val));
+  }
   cl_shutdown();
   return 0;
 }
